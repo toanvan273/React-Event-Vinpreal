769606f719cd4a56586307820f3f91eac6b0214f
truongvantoan <truongvantoan.neu@gmail.com>
2019-05-20 15:58:08 +0700

    style

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-
 import cameraon from './../images/ic_cameraon.png'
-
 import styled from 'styled-components'
 
 
-
 const RectangleCopy = styled.div`
-width: 100%;
-min-width: 1024px;
-height: 280px;
-background-color: #0a312f;
-display: flex;
-justify-content: space-around;
-align-items: center;
+    width: 100%;
+    min-width: 1024px;
+    height: 280px;
+    background-color: #0a312f;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `
 const Camera = styled.div`
     width: 260px;
@@ -42,35 +39,33 @@ const CardReset = styled(Camera)`
 class CameraShow extends Component {
     render() {
         return (
-            <>
-                <RectangleCopy>
-                    
-                    <CameraOn>
-                        <span>CAMERA ON</span>
-                        <div>
-                        <img src={cameraon} className="img-responsive" alt="made" />
-                        </div>
-                    </CameraOn>
-                    <CameraOff>
-                        <span>CAMERA OFF</span>
-                        <div>
-                        <img src={cameraon} className="img-responsive" alt="a" />
-                        </div>
-                    </CameraOff>
-                    <CameraClear>
-                        <span>CLEAR CAMERA</span>
-                        <div>
-                        <img src={cameraon} className="img-responsive" alt="se" />
-                        </div>
-                    </CameraClear>
-                    <CardReset>
-                        <span>RESET CARD</span>
-                        <div>
-                        <img src={cameraon} className="img-responsive" alt="he" />
-                        </div>
-                    </CardReset>
-                </RectangleCopy>
-            </>
+            <RectangleCopy>
+                <CameraOn>
+                    <span>CAMERA ON</span>
+                    <div>
+                        <img src={cameraon} className="img-responsive" alt="cameraon" />
+                    </div>
+                </CameraOn>
+                <CameraOff>
+                    <span>CAMERA OFF</span>
+                    <div>
+                        <img src={cameraon} className="img-responsive" alt="cameraoff" />
+                    </div>
+                </CameraOff>
+                <CameraClear>
+                    <span>CLEAR CAMERA</span>
+                    <div>
+                        <img src={cameraon} className="img-responsive" alt="cameraclear" />
+                    </div>
+                </CameraClear>
+                <CardReset>
+                    <span>RESET CARD</span>
+                    <div>
+                        <img src={cameraon} className="img-responsive" alt="card" />
+                    </div>
+                </CardReset>
+            </RectangleCopy>
+
 
         );
     }
